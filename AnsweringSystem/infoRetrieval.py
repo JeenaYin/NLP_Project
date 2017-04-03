b0cb7244fe94539c7d961557baa5ee502111c6c2
Sumedha Mehta <sumedha.mehta@gmail.com>
2017-04-03 02:47:25 -0400

    added code for finding target sentence

---
+++ AnsweringSystem/infoRetrieval.py
@@ -0,0 +1,61 @@
+import os
+import nltk
+from collections import defaultdict
+from fuzzywuzzy import fuzz
+
+questionList = []
+questionRanks = dict()
+THRESHHOLD = 50
+
+
+def getQuestions(questionfile):
+	with open(questionfile, "rt") as fileContents:
+		content = fileContents.read()
+		fileContents.close()
+
+	for question in content.splitlines():
+		questionList.append(question)
+
+
+	return questionList
+
+def getSentences(textfile):
+	with open(textfile, "rt") as fileContents:
+		content = fileContents.read()
+		fileContents.close()
+
+	possibleSentences = nltk.tokenize.sent_tokenize(content)
+	return possibleSentences
+
+
+def getTargetSentence(question, sentenceList):
+	questionDict = dict()
+	maxScore = 0
+	bestSentence = ""
+	for sentence in sentenceList: 
+		questionRank = fuzz.partial_ratio(question, sentence)
+		print(sentence)
+		print(questionRank)
+		if questionRank > maxScore:
+			bestSentence = sentence;
+			maxScore = questionRank
+	return bestSentence;
+
+def answerQuestion(targetSentence):
+	return targetSentence
+
+def main():
+	questions = getQuestions("SampleQuestion.txt")
+	sentenceList = getSentences("SampleDocument.txt")
+	for question in questions:
+		target = getTargetSentence(question, sentenceList)
+		print(question)
+		print(target)
+	return 1; 
+
+main();
+
+
+
+
+
