d133f4491edf9ff40d32295ac1f72603e4a5ca64
Philip Dominici <pdominic@andrew.cmu.edu>
2017-04-05 02:33:45 -0400

    Changes

--- AnsweringSystem/AnsweringMachine.py
+++ AnsweringSystem/AnsweringMachine.py
@@ -7,14 +7,13 @@
 
 
 import os, sys
+import re
 import string
 import math
 import numpy
 from nltk.tag.stanford import StanfordPOSTagger
 from nltk.parse.stanford import StanfordParser
 from nltk.parse.stanford import StanfordDependencyParser
-
-
 from nltk.tag import StanfordPOSTagger
 from nltk import word_tokenize
 
@@ -30,20 +29,56 @@ class AnsweringMachine(object):
 		# unpack question from txt file to string
 		with open(question, 'r') as f:
 			questionString = f.read()
+			questionString = clean(questionString)
 		self.question = questionString
 		self.document = document
+		self.wh = "who what when where"
 
-	def parse(self, sentence):
-		# parser = StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-		# # depParser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
-		# parsedSentence = parser.raw_parse(sentence)
-		return(parsedSentence)
+	# remove punctuation
+	def clean(self, s):
+		cleanString = s.translate(None, string.punctuation)
+		return(cleanString)
 
 	def answerQuestion(self):
 		# st = StanfordPOSTagger('english-bidirectional-distsim.tagger')
 		# tags = st.tag('What is the airspeed of an unladen swallow ?'.split())
-		text = pos_tagger.tag(word_tokenize("What's the airspeed of an unladen swallow ?"))
-		print(text)
+		#text = pos_tagger.tag(word_tokenize("What's the airspeed of an unladen swallow ?"))
+		#print(text)
+
+		# all the question tags we will consider
+		searchObj = re.findall(r'did|was|is|who|what|where|when|how', self.question, re.I)
+		qType = None
+		if (len(searchObj) == 1):
+			qType = searchObj[0]
+		else:
+			for (word in searchObj):
+				if ((word in self.wh) and (qType == None)):
+					qType = word
+			if (qType == None): qType = searchObj[0]
+		if (qType in self.wh):
+			self.answerWh
+		else: 
+			self.answerBinary
+
+	# consider binary (yes or no) questions
+	def answerBinary(self):
+		answer = False
+		phraseOfInterest = ""
+		# figure out the question-phrase we are interested in
+		questionRelations = None
+		if (len(questionRelations) > 1): # how to pick the relation of interest?
+			pass
+		# figure out the relations of the sentence
+		sentenceRelations = None
+		# check the relations of the sentence for the specific question phrase
+		for (relation in relations):
+			if (phraseOfInterest in relation): 
+				answer = True
+		return(answer)
+
+	# consider wh- (subject specific) questions
+	def answerWh(self, wh):
+		return()
 
 	def run(self):
 		self.answerQuestion()
