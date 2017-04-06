385cc56fdf3a4902ef3773b930fb4f6e45e13d3a
Philip Dominici <pdominic@andrew.cmu.edu>
2017-04-06 18:39:14 -0400

    Major changes to NER, getting close

--- AnsweringSystem/AnsweringMachine.py
+++ AnsweringSystem/AnsweringMachine.py
@@ -11,6 +11,7 @@ import re
 import string
 import math
 import numpy
+import nltk
 from nltk.tag.stanford import StanfordPOSTagger
 from nltk.parse.stanford import StanfordParser
 from nltk.parse.stanford import StanfordDependencyParser
@@ -25,19 +26,37 @@ pos_tagger = StanfordPOSTagger(model, jar, encoding='utf8')
 
 class AnsweringMachine(object):
 
-	def __init__(self, question, document):
+	def __init__(self, questionDoc, sentenceDoc):
 		# unpack question from txt file to string
-		with open(question, 'r') as f:
+		with open(questionDoc, 'r') as f:
 			questionString = f.read()
-			questionString = clean(questionString)
+			questionString = self.clean(questionString)
 		self.question = questionString
-		self.document = document
+		with open(sentenceDoc, 'r') as f:
+			sentenceString = f.read()
+			sentenceString = self.clean(sentenceString)
+		self.sentence = sentenceString
 		self.wh = "who what when where"
 
 	# remove punctuation
+	# input: STRING
+	# output: STRING which is stripped
 	def clean(self, s):
-		cleanString = s.translate(None, string.punctuation)
+		translator = str.maketrans('', '', string.punctuation)
+		cleanString = s.translate(translator)
 		return(cleanString)
+
+	# pass in a string to be tagged in NER
+	# input: a STRING, the sentence to be evaluated
+	# return: a LIST of TUPLES, each containing the word and its named entity
+	def ner(self, sentence):
+		#st = NERTagger('../stanford-ner/classifiers/all.3class.distsim.crf.ser.gz','../stanford-ner/stanford-ner.jar')
+		rawEntities = nltk.ne_chunk(nltk.pos_tag(word_tokenize(sentence))).pos() # tree object
+		entities = list()
+		for ent in rawEntities:
+			pureTuple = (ent[0][0], ent[1])
+			entities.append(pureTuple)
+		return(entities)
 
 	def answerQuestion(self):
 		# st = StanfordPOSTagger('english-bidirectional-distsim.tagger')
@@ -45,43 +64,54 @@ class AnsweringMachine(object):
 		#text = pos_tagger.tag(word_tokenize("What's the airspeed of an unladen swallow ?"))
 		#print(text)
 
-		# all the question tags we will consider
+		# all the question tags we will cownsider
 		searchObj = re.findall(r'did|was|is|who|what|where|when|how', self.question, re.I)
 		qType = None
 		if (len(searchObj) == 1):
 			qType = searchObj[0]
 		else:
-			for (word in searchObj):
+			for word in searchObj:
 				if ((word in self.wh) and (qType == None)):
 					qType = word
 			if (qType == None): qType = searchObj[0]
-		if (qType in self.wh):
-			self.answerWh
+		# note that we are working in lower case to identify question types
+		if (qType.lower() in self.wh):
+			self.answerWh(qType.lower())
 		else: 
-			self.answerBinary
+			self.answerBinary()
 
 	# consider binary (yes or no) questions
 	def answerBinary(self):
 		answer = False
 		phraseOfInterest = ""
 		# figure out the question-phrase we are interested in
-		questionRelations = None
+		questionRelations = []
 		if (len(questionRelations) > 1): # how to pick the relation of interest?
 			pass
 		# figure out the relations of the sentence
-		sentenceRelations = None
+		sentenceRelations = []
 		# check the relations of the sentence for the specific question phrase
-		for (relation in relations):
+		for relation in sentenceRelations:
 			if (phraseOfInterest in relation): 
 				answer = True
 		return(answer)
 
 	# consider wh- (subject specific) questions
 	def answerWh(self, wh):
-		return()
+		print(wh)
+		if (wh == "who"):
+			questionEntities = self.ner(self.question)
+			sentenceEntities = self.ner(self.sentence)
+		if (wh == "where"):
+			questionEntities = self.ner(self.question)
+			sentenceEntities = self.ner(self.sentence)
+		if (wh == "when"):
+			questionEntities = self.ner(self.question)
+			sentenceEntities = self.ner(self.sentence)
+		print(questionEntities)
 
 	def run(self):
 		self.answerQuestion()
 
 if __name__ == '__main__':
-	AnsweringMachine(question=sys.argv[1], document=sys.argv[2]).run()
+	AnsweringMachine(questionDoc=sys.argv[1], sentenceDoc=sys.argv[2]).run()
