cd1ff55e27da5588db6db88fd88eacffbad59793
Philip Dominici <pdominic@andrew.cmu.edu>
2017-04-17 18:16:22 -0400

    massive overhaul of inforetrieval

--- AnsweringSystem/infoRetrieval.py
+++ AnsweringSystem/infoRetrieval.py
@@ -5,79 +5,88 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from collections import defaultdict
 
 
-questionList = []
-THRESHOLD = 60
-wnl = WordNetLemmatizer()
+class InfoRetrieval(object):
 
-def getQuestions(questionfile):
-	with open(questionfile, "rt") as fileContents:
-		content = fileContents.read()
-		fileContents.close()
+	def __init__(self, questionDoc, document):
+		self.questionDoc = questionDoc
+		self.document = document
+		self.questionList = []
+		self.wordWeights = dict()
+		self.wnl = WordNetLemmatizer()
 
-	for question in content.splitlines():
-		questionList.append(question)
+	def getQuestions(self):
+		with open(self.questionDoc, "rt") as fileContents:
+			content = fileContents.read()
+			fileContents.close()
+		for question in content.splitlines():
+			self.questionList.append(question)
 
-	return questionList
 
+	def getSentences(self):
+		with open(self.document, "rt") as fileContents:
+			content = fileContents.read()
+			fileContents.close()
 
-def getSentences(textfile):
-	with open(textfile, "rt") as fileContents:
-		content = fileContents.read()
-		fileContents.close()
+		possibleSentences = nltk.tokenize.sent_tokenize(content)
+		return possibleSentences
 
-	possibleSentences = nltk.tokenize.sent_tokenize(content)
-	return possibleSentences
+	# Take a sentence and return its stripped down, lemmatized form
+	def lemmatize(self, s):
+		# remove punctuation in python 3.0+
+		translator = str.maketrans('', '', string.punctuation)
+		s2 = s.translate(translator)
+		lemString = ""
+		for word in s2.split():
+			lemString += self.wnl.lemmatize(word.lower())
+			lemString += " "
+		return lemString
 
-# Take a sentence and return its stripped down, lemmatized form
-def lemmatize(s):
-	# remove punctuation
-	s2 = s.translate(None, string.punctuation)
-	lemString = ""
-	for word in s2.split():
-		lemString += wnl.lemmatize(word.lower())
-		lemString += " "
-	return lemString
+	def weightWords(self):
+		weightDict = dict()
+		with open(self.document, "rt") as fullDoc:
+			f = fullDoc.read()
+			fullDoc.close()
+		# want to lemmatize to equalize word polymorphisms
+		allWords = self.lemmatize(f).split()
+		for word in allWords:
+			if (word in weightDict):
+				weightDict[word] = weightDict[word] + 1
+			else:
+				weightDict[word] = 1
+		return weightDict
 
-def weightWords(document):
-	weightDict = dict()
-	return weightDict
+	def getTargetSentence(self, question, sentenceList):
+		possibleTargetSentences = []
+		maxScore = 0
+		bestSentence = ""
+		lemQuestion = self.lemmatize(question)
+		qWords = lemQuestion.split()
+		for sentence in sentenceList:
+			lemSentence = self.lemmatize(sentence)
+			questionRank = 0
+			for word in lemSentence.split():
+				# if a word in the sentence matches one in the question, count it
+				if (word in qWords):
+					questionRank += 1.0/self.wordWeights[word]
+			if (questionRank > maxScore):
+				bestSentence = sentence
+				maxScore = questionRank
+		return bestSentence
 
-def getTargetSentence(question, sentenceList):
-	questionDict = dict()
-	possibleTargetSentences = []
-	maxScore = 0
-	bestSentence = ""
-	lemQuestion = lemmatize(question)
-	for sentence in sentenceList:
-		lemSentence = lemmatize(sentence)
-		questionRank = fuzz.token_set_ratio(lemQuestion, lemSentence)
-		if questionRank > THRESHOLD:
-			possibleTargetSentences += [sentence]
-		if questionRank > maxScore:
-			bestSentence = sentence;
-			maxScore = questionRank
-	if possibleTargetSentences == []:
-		return bestSentence;
-	else: 	
-		return possibleTargetSentences[0]
+	def run(self):
+		self.wordWeights = self.weightWords() # find word counts for inverse frequency calculations
+		self.getQuestions() # adds to questionlist
+		sentenceList = self.getSentences()
+		with open("ChosenSentences.txt", "w") as chosen:
+			chosen.write("")
+		for question in self.questionList:
+			target = self.getTargetSentence(question, sentenceList)
+			with open("ChosenSentences.txt", "a") as chosen:
+				chosen.write(target)
+				chosen.write("\n")
 
-def answerQuestion(targetSentence):
-	return targetSentence
-
-def main():
-	questions = getQuestions(sys.argv[1])
-	sentenceList = getSentences(sys.argv[2])
-	with open("ChosenSentences.txt", "w") as chosen:
-		chosen.write("")
-	for question in questions:
-		target = getTargetSentence(question, sentenceList)
-		with open("ChosenSentences.txt", "a") as chosen:
-			chosen.write(target)
-			chosen.write("\n")
-	return 1; 
-
-main();
-
+if __name__ == '__main__':
+	InfoRetrieval(questionDoc=sys.argv[1], document=sys.argv[2]).run()
 
 
 
